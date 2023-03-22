a62d316932f7b08ed9c318baaadec65ba1eeeb54
henryliu <17600047539@163.com>
2023-03-22 16:00:45 +0800

    update

--- src/api/v1/Rules/ApiAddRule.ts
+++ src/api/v1/Rules/ApiAddRule.ts
@@ -13,25 +13,57 @@ export default async function (call: ApiCall<ReqAddRule, ResAddRule>) {
 
     let time = new Date();
 
-    const rules = await AppDataSource.getRepository(ValidationRules).create()
-    rules.guild_id = call.req.guild_id;
-    rules.role_id = call.req.role_id;
-    rules.description = call.req.description;
-    rules.chain_type = call.req.chain_type;
-    rules.token_type = call.req.token_type;
-    rules.smart_contract_address = call.req.smart_contract_address;
-    rules.min_token_amount = call.req.min_token_amount;
-    rules.max_token_amount = call.req.max_token_amount;
-    const result = await AppDataSource.getRepository(ValidationRules).insert(rules)
-    if (result){
-        // Success
-        await call.succ({
-            time
-        });
-    }else {
-        // error
-        await call.error('empty');
-        return;
+
+
+
+    const last_rules = await AppDataSource.getRepository(ValidationRules).findOneBy(
+        {
+            guild_id:call.req.guild_id,
+            role_id:call.req.role_id
+        }
+    )
+    if (last_rules != null){
+        last_rules.description = call.req.description;
+        last_rules.chain_type = call.req.chain_type;
+        last_rules.token_type = call.req.token_type;
+        last_rules.smart_contract_address = call.req.smart_contract_address;
+        last_rules.min_token_amount = call.req.min_token_amount;
+        last_rules.max_token_amount = call.req.max_token_amount;
+        const result = await AppDataSource.getRepository(ValidationRules).save(last_rules)
+        if (result){
+            // Success
+            await call.succ({
+                time
+            });
+        }else {
+            // error
+            await call.error('empty');
+            return;
+        }
+    }else{
+        const rules = await AppDataSource.getRepository(ValidationRules).create()
+        rules.guild_id = call.req.guild_id;
+        rules.role_id = call.req.role_id;
+        rules.description = call.req.description;
+        rules.chain_type = call.req.chain_type;
+        rules.token_type = call.req.token_type;
+        rules.smart_contract_address = call.req.smart_contract_address;
+        rules.min_token_amount = call.req.min_token_amount;
+        rules.max_token_amount = call.req.max_token_amount;
+        const result = await AppDataSource.getRepository(ValidationRules).insert(rules)
+        if (result){
+            // Success
+            await call.succ({
+                time
+            });
+        }else {
+            // error
+            await call.error('empty');
+            return;
+        }
     }
+
+
+
 
 }
