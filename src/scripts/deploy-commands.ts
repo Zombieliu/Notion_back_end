1a3c3630b9e606c6eebe683b0c78697a7bbc64f1
henryliu <17600047539@163.com>
2023-03-24 23:01:12 +0800

    update

--- src/scripts/deploy-commands.ts
+++ src/scripts/deploy-commands.ts
@@ -1,7 +1,7 @@
 const { REST, Routes } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
-require('dotenv').config()
+// require('dotenv').config()
 
 
 const commands: any[] = [];
