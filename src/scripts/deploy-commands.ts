8df2cfd5929f115fc15b98bcf8c95c4c2312c8f6
henryliu <17600047539@163.com>
2023-03-24 23:15:37 +0800

    update

--- src/scripts/deploy-commands.ts
+++ src/scripts/deploy-commands.ts
@@ -1,25 +1,25 @@
 const { REST, Routes } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
-// require('dotenv').config()
+require('dotenv').config()
 
-
-const commands: any[] = [];
-// Grab all the command files from the commands directory you created earlier
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter((file: string) => file.endsWith('.ts'));
-
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
-}
-
-// Construct and prepare an instance of the REST module
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 // and deploy your commands!
 const deploy_commands = async (guild_id:string) => {
+    const commands: any[] = [];
+// Grab all the command files from the commands directory you created earlier
+    const commandsPath = path.join(__dirname, 'commands');
+    const commandFiles = fs.readdirSync(commandsPath).filter((file: string) => file.endsWith('.ts'));
+
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+    for (const file of commandFiles) {
+        const command = require(`./commands/${file}`);
+        commands.push(command.data.toJSON());
+    }
+    
+// Construct and prepare an instance of the REST module
+    const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
     console.log(process.env.CLIENTID)
     console.log("111111111111111111111",commands)
     try {
